ec8f3f165cab15432f84eba39417e2736636b442
Dharshan S <dharshans805@gmail.com>
2025-10-28 19:04:28 +0530

    Client Delete

--- script.ts
+++ script.ts
@@ -101,21 +101,51 @@ const prisma =new PrismaClient()
 //     console.log(user)
 // }
 
-//Update age
-async function main(){
-    
-    const user = await prisma.user.update({
-        where:{
-            email: "dacchu@teat1.com", //when v do an single update then it must be unique field and name is not unique
-        },
-        data:{
-            age: {increment: 5}, // incrementing age by 5
-            // age: {decrement: 5}, // decrementing age by 5
-            // age: {multiply: 2}, // multiplying age by 2
-            // age: {divide: 2}, // dividing age by 2
-        },
-    })
+// //Update age
+// async function main(){
 
+//     const user = await prisma.user.update({
+//         where:{
+//             email: "dacchu@teat1.com", //when v do an single update then it must be unique field and name is not unique
+//         },
+//         data:{
+//             age: {increment: 5}, // incrementing age by 5
+//             // age: {decrement: 5}, // decrementing age by 5
+//             // age: {multiply: 2}, // multiplying age by 2
+//             // age: {divide: 2}, // dividing age by 2
+//         },
+//     })
+
+//     console.log(user)
+// }
+
+
+//Client Delete
+// async function main(){
+//     const user = await prisma.user.delete({
+//         where:{
+//             //delete only delete one user and it must be a unique field
+//             email: "dacchu@teat1.com",
+//         }
+//     })
+//     console.log(user)
+// }
+
+
+//Delete Many
+// async function main(){
+//     const user = await prisma.user.deleteMany({
+//         where:{
+//             age:{gt:20}
+//         }
+//     })
+//     console.log(user)
+// }
+
+
+//to delete everything v should not mention anything in deleteMany()
+async function main(){
+    const user=await prisma.user.deleteMany()
     console.log(user)
 }
 
